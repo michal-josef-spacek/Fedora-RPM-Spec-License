863d4aa1533fb159d0f3f4182b40e82428a1ecd2
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-02-28 09:28:34 +0100

    Prepare for 0.04 version

--- License.pm
+++ License.pm
@@ -54,7 +54,7 @@ my $GRAMMAR2 = <<'END';
 	}
 END
 
-our $VERSION = 0.03;
+our $VERSION = 0.04;
 
 sub new {
 	my ($class, @params) = @_;
@@ -412,6 +412,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.03
+0.04
 
 =cut
--- t/Fedora-RPM-Spec-License/02-version.t
+++ t/Fedora-RPM-Spec-License/02-version.t
@@ -6,4 +6,4 @@ use Test::More 'tests' => 2;
 use Test::NoWarnings;
 
 # Test.
-is($Fedora::RPM::Spec::License::VERSION, 0.03, 'Version.');
+is($Fedora::RPM::Spec::License::VERSION, 0.04, 'Version.');
